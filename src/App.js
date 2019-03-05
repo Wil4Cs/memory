e493c02cd5f4656f9d6cbaf7818f07c1e8ffcd57
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2019-03-05 15:49:38 +0100

    Create App Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    handleCardClick(card) {
+        console.log(card, 'clicked');
+    }
+    render() {
+        return (
+            <div className="memory">
+                <Card
+                    card="😀"
+                    feedback="hidden"
+                    onClick={this.handleCardClick}
+                />
+                <Card
+                    card="🎉"
+                    feedback="justMatched"
+                    onClick={this.handleCardClick}
+                />
+                <Card
+                    card="💖"
+                    feedback="justMismatched"
+                    onClick={this.handleCardClick}
+                />
+                <Card
+                    card="🎩"
+                    feedback="visible"
+                    onClick={this.handleCardClick}
+                />
+                <Card
+                    card="🐶"
+                    feedback="hidden"
+                    onClick={this.handleCardClick}
+                />
+                <Card
+                    card="🐱"
+                    feedback="justMaonClick={this.handleCardClick}tched"
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
